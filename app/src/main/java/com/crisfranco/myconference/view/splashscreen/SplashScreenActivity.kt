d21049e2effff8a7ca983208cdbb2f5aca71e33d
Cristian David Franco Garcia <cristiandavidfrancogarcia@Cristians-MacBook-Pro.local>
2020-05-10 13:47:33 -0500

    first commit

---
+++ app/src/main/java/com/crisfranco/myconference/view/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.crisfranco.myconference.view.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.crisfranco.myconference.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
