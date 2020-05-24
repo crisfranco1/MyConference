9b235ec47f7f997250ef8948bd257bdcebab0c85
Cristian David Franco Garcia <cristiandavidfrancogarcia@gmail.com>
2020-05-24 12:27:28 -0500

    fin

--- app/src/main/java/com/crisfranco/myconference/view/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/crisfranco/myconference/view/activities/SplashScreenActivity.kt
@@ -1,13 +1,32 @@
 package com.crisfranco.myconference.view.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.crisfranco.myconference.R
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        val animation = AnimationUtils.loadAnimation(this, R.anim.animation)
+        imageViewLogoSplashScreen.startAnimation(animation)
+        val intent = Intent(this, MainActivity::class.java)
+        animation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+            }
+        })
     }
 }
